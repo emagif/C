209df4a7c15edadb112081baa7c150e70e7a7b4c
Emanuel Gifuni <s194407@student.pg.edu.pl>
2024-05-24 19:28:15 +0200

    C exercises

--- reallocFunction.c
+++ reallocFunction.c
@@ -3,4 +3,26 @@
 #include <malloc.h>
 
 
+int main() {
 
+    int* gradesArray;
+    int i, totalGrades;
+    scanf("%d", &totalGrades);
+    gradesArray = (int*)malloc(sizeof(int) * totalGrades);
+    if(gradesArray != NULL) {
+        printf("Allocation Succeded!\n");
+    }
+
+    else {
+        printf("Allocation Failed.\n");
+        exit(1);
+    }
+
+    for(i = 0; i < totalGrades; i++) {
+        scanf("%d", &gradesArray[i]);
+    }
+
+    
+
+    return 0;
+}
