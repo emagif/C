03c6f14890f49c16c66588683344ccb5761d7349
Emanuel Gifuni <s194407@student.pg.edu.pl>
2024-05-29 09:45:41 +0200

    C exercises

---
+++ arrayToArray.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+
+void* pointerArray(void** array, int size) {
+    
+}
+
+
+
+
+
+
+int main() {
+    int size;
+    int* ptr; 
+    printf("Enter the desired size of the array: ");
+    scanf("%d", &size);
+
+
+    return 0;
+}
