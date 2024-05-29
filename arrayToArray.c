68d0096a6d7219f24a829353b8ef877582c490a1
Emanuel Gifuni <s194407@student.pg.edu.pl>
2024-05-29 11:54:21 +0200

    C exercises

--- arrayToArray.c
+++ arrayToArray.c
@@ -3,20 +3,27 @@
 #include <malloc.h>
 
 
-void* pointerArray(void** array, int size) {
-    
+void pointerArray(int** p, int size) {
+
+    int i;
+    *p = (int*)malloc(sizeof(int) * size);
+    for(i = 0; i < size; i++) {
+        printf("Enter value: ");
+        scanf("%d", &(*p)[i]);
+    }
+
 }
-
-
-
 
 
 
 int main() {
     int size;
     int* ptr; 
+
     printf("Enter the desired size of the array: ");
     scanf("%d", &size);
+
+    pointerArray(&ptr, size);
 
 
     return 0;
