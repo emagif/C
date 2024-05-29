0c4854994b1f879403a9112bdda85a5ebd8d7c29
Emanuel Gifuni <s194407@student.pg.edu.pl>
2024-05-29 21:09:56 +0200

    C exercises

--- newArrayNoDuplicates.c
+++ newArrayNoDuplicates.c
@@ -5,13 +5,28 @@
 
 
 
-    
+int* noDuplicates(int* ptr, int size) {
+    int i, j;
+    int count = 1;
+    int* noDuplexArray;
+
+    for(i = 0; i < size - 1; i++) {
+        if(ptr[i]!=ptr[i+1]) {
+            count++;
+        }
+    }
+    noDuplexArray = (int*)malloc(sizeof(int) * count);
+    for(i = 0; i < size - 1; i++) {
+        if(ptr[i]!=ptr[i]) {
+            noDuplexArray[i] = ptr[i];
+        }
+    }
+}
 
 
 
 int main() {
-    
-
+    int originalArray[] = {1, 3, 3, 5, 6, 7, 7, 7, 8, 12, 12};
 
     return 0;
 }
