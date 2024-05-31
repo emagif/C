05a98b94e3028942db3a1b0c28093ce62e853549
Emanuel Gifuni <s194407@student.pg.edu.pl>
2024-05-31 17:06:27 +0200

    C exercises

--- upperLowerMatrix.c
+++ upperLowerMatrix.c
@@ -3,6 +3,30 @@
 #include <malloc.h>
 
 
+// Function responsible for creating the lower triangular matrix
+int** createLowerTriangularMatrix(int n) { // n indicates number of rows and number of columns in the matrix
+    int** a;
+    int i;
+    a = (int**)malloc(sizeof(int*) * n);
+    for(i = 0; i < n; i++) {
+        a[i] = (int*)malloc(sizeof(int) * (i + 1));
+    }
+    return a;
+}
+
+
+// Function responsible for creating the upper triangular matrix
+int** createUpperTriangularMatrix(int n) { // n indicates number of rows and number of columns in the matrix
+    int** a;
+    int i; 
+    a = (int**)malloc(sizeof(int*) * n);
+    for(i = 0; i < n; i++) {
+        a[i] = (int*)malloc(sizeof(int) * (n - 1));
+    }
+    return a;
+}
+
+
 
 int main() {
     int** a;
