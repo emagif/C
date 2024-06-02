7e01b4dfe4214d2a9c0eabbf3c08b1888570505b
Emanuel Gifuni <s194407@student.pg.edu.pl>
2024-06-02 16:54:13 +0200

    C exercises

--- returningString.c
+++ returningString.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
+#include <string.h>
 
 
 
+char* strConcat(char* originalStr);
 
 
 
@@ -13,4 +15,22 @@ int main() {
 
 
     return 0;
+}
+
+
+
+
+char* strConcat(char* originalStr) {
+    int i; 
+    char* newStr = NULL; 
+    int lengthOriginal = strlen(originalStr);
+    newStr = (char*)malloc((sizeof(char) * lengthOriginal * 2) + 1);
+    for(i = 0; i < lengthOriginal; i++) {
+        newStr[i] = originalStr[i];
+        newStr[i + lengthOriginal] = originalStr[i];
+    }
+    newStr[i + lengthOriginal] = '\0';
+
+    return newStr;
+
 }
