f9f18f9ac382aa725003871b49cfe0b2c99dc5ee
Emanuel Gifuni <s194407@student.pg.edu.pl>
2024-06-02 17:04:57 +0200

    C exercises

---
+++ stringPointers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <malloc.h>
+
+
+
+
+int main() {
+
+    char charsArray[] = {'h', 'e', 'l', 'l', 'o', '\0'};
+    char str[] = "Hello";
+
+    char* tempPtr; 
+    tempPtr = charsArray;
+    tempPtr[0] = 'G';
+    printf("%c", charsArray[0]);
+
+
+    return 0;
+}
