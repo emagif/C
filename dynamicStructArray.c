f0ff78ff9cf258c89414ab0421462824d62d3250
Emanuel Gifuni <s194407@student.pg.edu.pl>
2024-06-07 09:09:02 +0200

    C exercises

--- dynamicStructArray.c
+++ dynamicStructArray.c
@@ -6,3 +6,37 @@
 
 
 
+typedef struct point {
+    int x; 
+    int y; 
+}Point;
+
+
+int main() {
+    Point* pointsArray; // allocated dynamically
+    int totalPoints; 
+    int i;
+
+
+    printf("Enter the total number of points: ");
+    scanf("%d", &totalPoints);
+
+    pointsArray = (Point*)malloc(sizeof(Point) * totalPoints);
+
+    for(i = 0; i < totalPoints; i++) {
+        printf("Enter coordinate 'x' for point #%d\t", i+1);
+        scanf("%d", &pointsArray[i].x);
+
+        printf("Enter coordinate 'y' for point #%d\t", i+1);
+        scanf("%d", &pointsArray[i].y);
+    }
+
+    for(i = 0; i < totalPoints; i++) {
+        printf("Coordinates of point #%d = (%d, %d)", i+1, pointsArray[i].x, pointsArray[i].y);
+        printf("\n");
+    }
+
+
+
+    return 0;
+}
