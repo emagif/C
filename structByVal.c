a57f3565e507055dc9b5be0a14742df6ab01fdd2
Emanuel Gifuni <s194407@student.pg.edu.pl>
2024-06-07 17:24:28 +0200

    C exercises

---
+++ structByVal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+
+
+typedef struct employee {
+    char name[10];
+    float age; 
+    int id;
+}Employee;
+
+
+int main() {
+
+
+
+
+    return 0;
+}
