3bc3d67c3ad91704a46efa61eacbee3160b56f6e
Emanuel Gifuni <s194407@student.pg.edu.pl>
2024-06-08 09:44:40 +0200

    C exercises

--- solveDMA.c
+++ solveDMA.c
@@ -13,22 +13,45 @@ typedef struct employee {
 
 
 Employee* createEmployeesArray(int totalElements) {
+    
     Employee* employeesArr;
     int i;
     employeesArr = (Employee*)malloc(sizeof(Employee) * totalElements);
     for(i = 0; i < totalElements; i++) {
         printf("Enter the name of the %d employee", i + 1);
-        scanf("%s", employeesArr[i].name);
+        scanf("%s", &employeesArr[i].name);
         printf("\n");
 
         printf("Enter the age of the %d employee", i + 1);
-        scanf("%f", employeesArr[i].age);
+        scanf("%f", &employeesArr[i].age);
         printf("\n");
 
         printf("Enter the id of the %d employee", i + 1);
-        scanf("%d", employeesArr[i].id);
+        scanf("%d", &employeesArr[i].id);
         printf("\n");
     }
+
+    return employeesArr;
+}
+
+void updateEmployeesArray(int size, Employee** employeesArray) {
+    
+    int i; 
+    employeesArray = (Employee*)malloc(sizeof(Employee) * size);
+    for(i = 0; i < size; i++) {
+        printf("Enter the name of the %d employee", i + 1);
+        scanf("%s", &employeesArray[i].name);
+        printf("\n");
+
+        printf("Enter the age of the %d employee", i + 1);
+        scanf("%f", &employeesArray[i].age);
+        printf("\n");
+
+        printf("Enter the name of the %d employee", i + 1);
+        scanf("%d", &employeesArray[i].id);
+        printf("\n");
+    }
+    *empPtr = employeesArray;
 }
 
 
